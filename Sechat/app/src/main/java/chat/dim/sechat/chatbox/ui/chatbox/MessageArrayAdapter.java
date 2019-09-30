771306572c8af164c28adc8e0eecd9900f7b8396
Moky <albert.moky@gmail.com>
2019-10-01 01:04:54 +0800

    show command message

--- Sechat/app/src/main/java/chat/dim/sechat/chatbox/ui/chatbox/MessageArrayAdapter.java
+++ Sechat/app/src/main/java/chat/dim/sechat/chatbox/ui/chatbox/MessageArrayAdapter.java
@@ -12,8 +12,10 @@ import android.widget.TextView;
 import java.util.List;
 
 import chat.dim.common.Facebook;
+import chat.dim.dkd.Content;
 import chat.dim.dkd.InstantMessage;
 import chat.dim.mkm.ID;
+import chat.dim.protocol.Command;
 import chat.dim.protocol.TextContent;
 import chat.dim.sechat.R;
 
@@ -36,6 +38,7 @@ public class MessageArrayAdapter extends ArrayAdapter<InstantMessage> {
             view = LayoutInflater.from(getContext()).inflate(resId, null);
             viewHolder = new ViewHolder();
             viewHolder.leftLayout = (LinearLayout) view.findViewById(R.id.received_msg);
+            viewHolder.centerLayout = (LinearLayout) view.findViewById(R.id.cmd_msg);
             viewHolder.rightLayout = (LinearLayout) view.findViewById(R.id.sent_msg);
             view.setTag(viewHolder);
         } else {
@@ -46,16 +49,25 @@ public class MessageArrayAdapter extends ArrayAdapter<InstantMessage> {
         MsgType type = ChatboxViewModel.getType(iMsg);
         if (MsgType.SENT == type) {
             viewHolder.leftLayout.setVisibility(View.GONE);
+            viewHolder.centerLayout.setVisibility(View.GONE);
             viewHolder.rightLayout.setVisibility(View.VISIBLE);
             viewHolder.avatarView = view.findViewById(R.id.right_avatar);
             viewHolder.nameView = view.findViewById(R.id.right_name);
             viewHolder.msgView = view.findViewById(R.id.right_message);
         } else if (MsgType.RECEIVED == type) {
             viewHolder.leftLayout.setVisibility(View.VISIBLE);
+            viewHolder.centerLayout.setVisibility(View.GONE);
             viewHolder.rightLayout.setVisibility(View.GONE);
             viewHolder.avatarView = view.findViewById(R.id.left_avatar);
             viewHolder.nameView = view.findViewById(R.id.left_name);
             viewHolder.msgView = view.findViewById(R.id.left_message);
+        } else if (MsgType.COMMAND == type) {
+            viewHolder.leftLayout.setVisibility(View.GONE);
+            viewHolder.centerLayout.setVisibility(View.VISIBLE);
+            viewHolder.rightLayout.setVisibility(View.GONE);
+            viewHolder.avatarView = null;
+            viewHolder.nameView = null;
+            viewHolder.msgView = view.findViewById(R.id.center_message);
         }
         showMessage(iMsg, viewHolder);
 
@@ -65,26 +77,40 @@ public class MessageArrayAdapter extends ArrayAdapter<InstantMessage> {
     private void showMessage(InstantMessage iMsg, ViewHolder viewHolder) {
         Facebook facebook = Facebook.getInstance();
         ID sender = facebook.getID(iMsg.envelope.sender);
+        Content content = iMsg.content;
 
         // avatar
 
         // name
-        String name = facebook.getNickname(sender);
-        viewHolder.nameView.setText(name);
+        if (viewHolder.nameView != null) {
+            String name = facebook.getNickname(sender);
+            viewHolder.nameView.setText(name);
+        }
 
         // message
-        if (iMsg.content instanceof TextContent) {
-            TextContent content = (TextContent) iMsg.content;
-            viewHolder.msgView.setText(content.getText());
+        if (content instanceof TextContent) {
+            TextContent textContent = (TextContent) content;
+            viewHolder.msgView.setText(textContent.getText());
+        } else if (content instanceof Command) {
+            Command cmd = (Command) content;
+            Object text = cmd.get("text");
+            if (text == null) {
+                text = cmd.get("message");
+                if (text == null) {
+                    text = cmd.command;
+                }
+            }
+            viewHolder.msgView.setText((String) text);
         }
     }
 
     class ViewHolder {
-        LinearLayout leftLayout;
-        LinearLayout rightLayout;
+        LinearLayout leftLayout = null;
+        LinearLayout rightLayout = null;
+        LinearLayout centerLayout = null;
 
-        ImageView avatarView;
-        TextView nameView;
-        TextView msgView;
+        ImageView avatarView = null;
+        TextView nameView = null;
+        TextView msgView = null;
     }
 }
